2350400c910f3d4623fa9541668f69151d67f858
Luis Enrique Jr. Pinto Fuentes <91229941+LuisPinto9@users.noreply.github.com>
2023-12-09 11:46:09 -0500

    Validacion de citas

--- controllers/appointment.js
+++ controllers/appointment.js
@@ -29,38 +29,81 @@ module.exports = {
     const patient = await Patient.findById(id);
     if (patient) {
       try {
-        const appointment = new Appointment(req.body);
+        let found = await Appointment.findOne({ number: req.body.number });
+        if (!found) {
+          const dateRegex = /^(0[0-9]|1[0-9]|2[0-3]):[0-5][0-9]$/;
+          const isDateValid = !isNaN(Date.parse(req.body.date));
 
-        appointment.patient = patient;
+          if (
+            isDateValid &&
+            dateRegex.test(req.body.hour) &&
+            Date.parse(req.body.date) >= Date.now()
+          ) {
+            const appointment = new Appointment(req.body);
+            appointment.patient = patient;
 
-        const result = await appointment.save();
+            const result = await appointment.save();
 
-        patient.appointments.push(appointment);
+            patient.appointments.push(appointment);
 
-        await patient.save();
+            await patient.save();
 
-        return res.status(200).json({ state: true, data: result });
+            return res.status(200).json({ state: true, data: result });
+          } else {
+            return res.status(500).json({
+              state: false,
+              error: "La fecha o la hora no son válidas",
+            });
+          }
+        } else {
+          return res
+            .status(500)
+            .json({ state: false, error: "Ya hay una cita con ese número" });
+        }
       } catch (error) {
         return res.status(500).json({ state: false, error: error.message });
       }
     } else {
-      return res.status(200).json({ state: false, error: " No Existe" });
+      return res
+        .status(200)
+        .json({ state: false, error: "El paciente no existe" });
     }
   },
   update: async (req, res) => {
     const { idAppointment } = req.params;
-    const { numero,name,fecha,hora, medic, patients } = req.body;
+    const { number, name, hour, date, specialist, patient } = req.body;
+
     try {
+      const isValidDate = !isNaN(Date.parse(date));
+      const isHourFormatValid = /^(0[0-9]|1[0-9]|2[0-3]):[0-5][0-9]$/.test(
+        hour
+      );
+
+      if (!isValidDate || !isHourFormatValid) {
+        return res
+          .status(400)
+          .json({ state: false, error: "Formato de fecha o hora incorrecto" });
+      }
+
       const updatedAppointment = await Appointment.findByIdAndUpdate(
         idAppointment,
-        { numero: numero, name: name, hora:hora,fecha:fecha,medic:medic, patients: patients },
+        {
+          number: number,
+          name: name,
+          hour: hour,
+          date: date,
+          specialist: specialist,
+          patient: patient,
+        },
         { new: true }
       );
+
       if (!updatedAppointment) {
         return res
           .status(404)
-          .json({ state: false, message: "cita no encontrada" });
+          .json({ state: false, message: "Cita no encontrada" });
       }
+
       return res.status(200).json({ state: true, data: updatedAppointment });
     } catch (error) {
       return res.status(500).json({ state: false, error: error.message });
@@ -69,12 +112,27 @@ module.exports = {
   drop: async (req, res) => {
     const { idAppointment } = req.params;
     try {
-      const deletedAppointment = await Appointment.findByIdAndDelete(idAppointment);
+      const deletedAppointment = await Appointment.findByIdAndDelete(
+        idAppointment
+      );
       if (!deletedAppointment) {
         return res
           .status(404)
-          .json({ state: false, message: "cita no encontrado" });
+          .json({ state: false, message: "Cita no encontrada" });
       }
+
+      const updatedPatient = await Patient.findOneAndUpdate(
+        { appointments: idAppointment },
+        { $pull: { appointments: idAppointment } },
+        { new: true }
+      );
+
+      if (!updatedPatient) {
+        return res
+          .status(404)
+          .json({ state: false, message: "Paciente no encontrado" });
+      }
+
       return res.status(200).json({ state: true, data: deletedAppointment });
     } catch (error) {
       return res.status(500).json({ state: false, error: error.message });
